524c7c34b77e49767e72d70b881d3539be8132b7
Pavan Wadawadagi <pavansdoc@gmail.com>
2020-09-08 14:19:40 +0530

    Clean up

--- backend/src/test/java/com/stock/admin/StockAdminApplicationTests.java
+++ backend/src/test/java/com/stock/admin/StockAdminApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.stock.admin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class StockAdminApplicationTests {
 
 	@Test
 	void contextLoads() {
